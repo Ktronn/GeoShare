da81ec3bce4f7098809059cb07af1b4fc17096c4
Victor M Perez Querol <viticopq12@gmail.com>
2019-01-22 13:35:51 -0500

    Drawer activity added plus widget launches street view with a default long and lat.

--- app/src/main/java/pp2/fullsailuniversity/geoshare/GeoFindW.java
+++ app/src/main/java/pp2/fullsailuniversity/geoshare/GeoFindW.java
@@ -5,20 +5,29 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.widget.RemoteViews;
 
 public class GeoFindW extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int appWidgetID: appWidgetIds)
-        {
-            Intent intent = new Intent(context,MapsActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+        for (int appWidgetID : appWidgetIds) {
+
+
+            // Create a Uri from an intent string. Use the result to create an Intent.
+            Uri gmmIntentUri = Uri.parse("google.streetview:cbll=28.5943978,-81.3039675");
+
+           // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+          // Make the Intent explicit by setting the Google Maps package
+            mapIntent.setPackage("com.google.android.apps.maps");
+
+            //Intent intent = new Intent(context, MapsActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mapIntent, 0);
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.geo_widget);
-            views.setOnClickPendingIntent(R.id.geo_find_button,pendingIntent);
-            appWidgetManager.updateAppWidget(appWidgetID,views);
+            views.setOnClickPendingIntent(R.id.geo_find_button, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetID, views);
         }
     }
 }

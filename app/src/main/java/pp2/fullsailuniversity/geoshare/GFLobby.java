ecf4a8b70b46fc4e40343d9c2b7c89f724c237c3
Victor M Perez Querol <viticopq12@gmail.com>
2019-01-24 10:13:36 -0500

    Test to find current location is being implemented in main activity(GFLobby)

--- app/src/main/java/pp2/fullsailuniversity/geoshare/GFLobby.java
+++ app/src/main/java/pp2/fullsailuniversity/geoshare/GFLobby.java
@@ -1,8 +1,15 @@
 package pp2.fullsailuniversity.geoshare;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +20,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import static android.support.v4.content.ContextCompat.getSystemService;
+
+public class GFLobby extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        LocationListener {
+
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,22 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -98,4 +126,24 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-}
+
+           @Override
+           public void onLocationChanged(Location location) {
+
+           }
+
+           @Override
+           public void onStatusChanged(String provider, int status, Bundle extras) {
+
+           }
+
+           @Override
+           public void onProviderEnabled(String provider) {
+
+           }
+
+           @Override
+           public void onProviderDisabled(String provider) {
+
+           }
+       }
--- app/src/main/java/pp2/fullsailuniversity/geoshare/GeoFindW.java
+++ app/src/main/java/pp2/fullsailuniversity/geoshare/GeoFindW.java
@@ -8,18 +8,25 @@ import android.content.Intent;
 import android.net.Uri;
 import android.widget.RemoteViews;
 
+
+
 public class GeoFindW extends AppWidgetProvider {
+
+
+
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetID : appWidgetIds) {
 
 
-            // Create a Uri from an intent string. Use the result to create an Intent.
-            Uri gmmIntentUri = Uri.parse("google.streetview:cbll=28.5943978,-81.3039675");
 
-           // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+            // Create a Uri from an intent string. Use the result to create an Intent.
+            Uri gmmIntentUri = Uri.parse("google.streetview:cbll=longitude,latitude");
+
+            // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
             Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-          // Make the Intent explicit by setting the Google Maps package
+            // Make the Intent explicit by setting the Google Maps package
             mapIntent.setPackage("com.google.android.apps.maps");
 
             //Intent intent = new Intent(context, MapsActivity.class);
@@ -30,4 +37,9 @@ public class GeoFindW extends AppWidgetProvider {
             appWidgetManager.updateAppWidget(appWidgetID, views);
         }
     }
+
+
+
+
+
 }
